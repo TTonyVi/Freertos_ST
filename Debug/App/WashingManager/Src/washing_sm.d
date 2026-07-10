87b32020cdf67dd43a429bc95972195de66ae4f5
TTonyVi <146604912+TTonyVi@users.noreply.github.com>
2026-07-10 21:21:29 +0700

    Ignore build files

--- Debug/App/WashingManager/Src/washing_sm.d
+++
@@ -1,6 +0,0 @@
-App/WashingManager/Src/washing_sm.o: \
- ../App/WashingManager/Src/washing_sm.c \
- C:/Users/Dell/STM32CubeIDE/workspace_1.19.0/Freertos/App/WashingManager/Inc/washing_sm.h \
- C:/Users/Dell/STM32CubeIDE/workspace_1.19.0/Freertos/App/Common/Inc/app_types.h
-C:/Users/Dell/STM32CubeIDE/workspace_1.19.0/Freertos/App/WashingManager/Inc/washing_sm.h:
-C:/Users/Dell/STM32CubeIDE/workspace_1.19.0/Freertos/App/Common/Inc/app_types.h:
